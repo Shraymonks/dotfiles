dccd27a4166866f71ee81a34e7eba062f2f744a0
Raymond Ha <raymond@shraymonks.com>
2024-06-06 01:06:35 -0700

    Support trouble.nvim v3

--- dot_config/nvim/lua/plugins/mason-lspconfig.lua
+++ dot_config/nvim/lua/plugins/mason-lspconfig.lua
@@ -20,10 +20,7 @@ return {
         -- Mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
         local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
         vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
         vim.keymap.set(
           'n',
@@ -40,7 +37,6 @@ return {
         vim.keymap.set('n', '<space>wl', function()
           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
         end, bufopts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
         vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
         vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
         vim.keymap.set('n', '<space>f', function()
--- dot_config/nvim/lua/plugins/trouble.lua
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -1,45 +1,65 @@
 return {
   {
     'folke/trouble.nvim',
+    opts = {},
     dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      vim.keymap.set(
-        'n',
+    cmd = 'Trouble',
+    keys = {
+      {
         '<leader>xx',
-        '<cmd>TroubleToggle<cr>',
-        { silent = true, noremap = true }
-      )
-      vim.keymap.set(
-        'n',
-        '<leader>xw',
-        '<cmd>TroubleToggle workspace_diagnostics<cr>',
-        { silent = true, noremap = true }
-      )
-      vim.keymap.set(
-        'n',
-        '<leader>xd',
-        '<cmd>TroubleToggle document_diagnostics<cr>',
-        { silent = true, noremap = true }
-      )
-      vim.keymap.set(
-        'n',
-        '<leader>xl',
-        '<cmd>TroubleToggle loclist<cr>',
-        { silent = true, noremap = true }
-      )
-      vim.keymap.set(
-        'n',
-        '<leader>xq',
-        '<cmd>TroubleToggle quickfix<cr>',
-        { silent = true, noremap = true }
-      )
-      vim.keymap.set(
-        'n',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+      {
+        'gD',
+        '<cmd>Trouble lsp_declarations toggle focus=true<cr>',
+        desc = 'LSP Declarations',
+      },
+      {
+        'gd',
+        '<cmd>Trouble lsp_definitions toggle focus=true<cr>',
+        desc = 'LSP Definitions',
+      },
+      {
+        'gi',
+        '<cmd>Trouble lsp_implementations toggle focus=true<cr>',
+        desc = 'LSP Implementations',
+      },
+      {
         'gr',
-        '<cmd>TroubleToggle lsp_references<cr>',
-        { silent = true, noremap = true }
-      )
-      require('trouble').setup()
-    end,
+        '<cmd>Trouble lsp_references toggle focus=true<cr>',
+        desc = 'LSP References',
+      },
+      {
+        'gt',
+        '<cmd>Trouble lsp_type_definitions toggle focus=true<cr>',
+        desc = 'LSP Type Definitions',
+      },
+    },
   },
 }
