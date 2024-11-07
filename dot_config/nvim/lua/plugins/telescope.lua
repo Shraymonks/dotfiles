f54fa50cf06d836c80702dcb0c9a1c7573a0f5cf
Raymond Ha <raymond@shraymonks.com>
2024-11-06 21:54:03 -0500

    Rebind telescope buffer map

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -32,7 +32,7 @@ return {
       }
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<C-b>', builtin.buffers, {})
+      vim.keymap.set('n', '<C-k>', builtin.buffers, {})
       vim.keymap.set('n', '<C-j>', builtin.live_grep, {})
     end,
   },
