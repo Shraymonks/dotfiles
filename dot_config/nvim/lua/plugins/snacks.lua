c1574dde76eed3e45320d4177feb0458f8aedbeb
Raymond Ha <raymond@shraymonks.com>
2025-06-24 21:33:24 -0400

    Exclude lock files from grep

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,11 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      exclude = {
+        "package-lock.json",
+        "pnpm-lock.yaml",
+        "yarn.lock",
+      },
       hidden = true,
       sources = {
         files = {
