be34bc19b19c3716cee722426ba06ef0da5acfca
Raymond Ha <raymond@shraymonks.com>
2025-08-06 15:41:50 -0400

    Dynamic file trunacate width for snacks picker

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -11,6 +11,27 @@ return {
       sources = {
         files = {
           hidden = true,
+          actions = {
+            calculate_file_truncate_width = function(self)
+              local width = self.list.win:size().width
+              self.opts.formatters.file.truncate = width - 6
+            end,
+          },
+          win = {
+            list = {
+              on_buf = function(self)
+                self:execute("calculate_file_truncate_width")
+              end,
+            },
+            preview = {
+              on_buf = function(self)
+                self:execute("calculate_file_truncate_width")
+              end,
+              on_close = function(self)
+                self:execute("calculate_file_truncate_width")
+              end,
+            },
+          },
         },
       },
     },
