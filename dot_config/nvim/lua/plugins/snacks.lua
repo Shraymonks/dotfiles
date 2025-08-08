43c07a1bfc73a07b504c9d58a33dd26e3a8ef682
Raymond Ha <raymond@shraymonks.com>
2025-08-07 20:57:29 -0400

    Fix truncate for buffers

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,3 +1,28 @@
+local actions = function(offset)
+  return {
+    calculate_file_truncate_width = function(self)
+      local width = self.list.win:size().width
+      self.opts.formatters.file.truncate = width - offset
+    end,
+  }
+end
+
+local win = {
+  list = {
+    on_buf = function(self)
+      self:execute("calculate_file_truncate_width")
+    end,
+  },
+  preview = {
+    on_buf = function(self)
+      self:execute("calculate_file_truncate_width")
+    end,
+    on_close = function(self)
+      self:execute("calculate_file_truncate_width")
+    end,
+  },
+}
+
 return {
   "folke/snacks.nvim",
   opts = {
@@ -9,29 +34,14 @@ return {
       },
       hidden = true,
       sources = {
+        buffers = {
+          actions = actions(16),
+          win = win,
+        },
         files = {
           hidden = true,
-          actions = {
-            calculate_file_truncate_width = function(self)
-              local width = self.list.win:size().width
-              self.opts.formatters.file.truncate = width - 6
-            end,
-          },
-          win = {
-            list = {
-              on_buf = function(self)
-                self:execute("calculate_file_truncate_width")
-              end,
-            },
-            preview = {
-              on_buf = function(self)
-                self:execute("calculate_file_truncate_width")
-              end,
-              on_close = function(self)
-                self:execute("calculate_file_truncate_width")
-              end,
-            },
-          },
+          actions = actions(6),
+          win = win,
         },
       },
     },
